f423b4a5fff1c5fdd1a1de075d2c3c8b05538a5a
Jody Mardika <jody_mardika_24rpl@student.smktelkom-mlg.sch.id>
2016-09-11 08:29:59 +0700

    Langkah Pertama  Pengaturan design awal

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5015/androidaplication/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5015/androidaplication/MainActivity.java
@@ -2,6 +2,8 @@ package id.sch.smktelkom_mlg.tugas01.xirpl5015.androidaplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.button1);
+        Button btn2 = (Button) findViewById(R.id.button2);
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                moveTaskToBack(true);
+            }
+        });
+
+
     }
 }
